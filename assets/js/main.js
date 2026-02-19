b0402ad1b8c3253fa8b65d7cb4e6022622b1ad9d
Kathleen-Chen05 <Kathleenchen2005@gmail.com>
2026-02-19 00:33:00 -0600

    Enhance main.js with interactive h1 element
    Add event listeners for mouseover and mouseout on targetElement.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
 // main.js
 
 console.log("Hello, World!");
+
+let targetElement = document.querySelector("h1");
+console.log(targetElement.outerHTML);
+targetElement.addEventListener("mouseover", function() {
+  console.log(this.innerText);
+  this.innerText = "SHOP";
+  this.style = "cursor: wait";
+});
+
+targetElement.addEventListener("mouseout", function() {
+  this.innerText = "Target";
+  this.style = "cursor: pointer";
+});
